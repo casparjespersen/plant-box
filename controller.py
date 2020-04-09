590ce79d5b4e0510b1322773c468f664e38a5c84
Caspar Jespersen <casparjespersen@me.com>
2020-04-09 09:39:01 +0200

    Added output pin for displaying

--- controller.py
+++ controller.py
@@ -3,18 +3,25 @@ from time import sleep
 from datetime import datetime
 
 
-PIN = 11
+PIN_IN = 11
+PIN_OUT = 13
 
 
 def setup():
+    """ Set the Raspberry Pi IO configuraton. """
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(PIN_IN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(PIN_OUT, GPIO.OUT)
+    GPIO.output(PIN_OUT, GPIO.LOW)
+
 
 def listen_for_press(fs: int):
+    """ Continuously listen for a button push, and yield the push duration
+        after detection. """
     state = 0
     while True:
-        value = bool(GPIO.input(PIN))
+        value = GPIO.input(PIN_IN)
         if state:
             if value:
                 state += 1
@@ -25,14 +32,29 @@ def listen_for_press(fs: int):
             state = 1
         sleep(1/fs)
 
+
+def show_output(duration: float):
+    GPIO.output(PIN_OUT, GPIO.HIGH)
+    sleep(duration)
+    GPIO.output(PIN_OUT, GPIO.LOW)
+
+
+def blink(pulse: float, reps: int):
+    for _ in range(reps):
+        show_output(pulse)
+        sleep(pulse)
+
+
 def main():
     setup()
     try:
         for duration in listen_for_press(fs=100):
-            print("keypress", duration)
+            print("Keypress detected.", duration)
+            blink(0.1, reps=int(duration)+1)
     except KeyboardInterrupt:
         pass
     GPIO.cleanup()
+
 
 if __name__ == "__main__":
     main()
